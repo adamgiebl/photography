5cec09d60cf27cd4f2d3903d7cfdb6452c56d0c9
Adam <adamgiebl@gmail.com>
2020-03-08 22:01:06 +0100

    modal

---
+++ README.md
@@ -0,0 +1,5 @@
+# Photography tutorial - KEA project
+
+# Docs
+
+[XD prototype](https://xd.adobe.com/view/70f68f47-c92c-4282-4aa0-edee35303aa8-06c7/screen/000bc9fe-6bb9-4b15-a023-62b07a015422/)
--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import Carousel, { Modal, ModalGateway } from 'react-images';
+import image from "../images/moodboard.png";
 
 function Footer() {
+
+	const [ isModalOpen, toggleModal ] = useState(false);
+
   return (
     <footer className="section">
-        <div className="page footer">
-            <span className="smaller">Grab a camera</span>
-            <span className="bigger">and go!</span>
-            <div style={{ display: 'flex', alignItems: 'center', bottom: '8vh', position: 'absolute' }}>
-            <a class="button" href="../images/moodboard.png" style={{ marginRight: '20px' }}>moodboard</a>
-            <a href="https://adamgiebl.best" target="_blank">adamgiebl.best</a>
-            </div>
+      <div className="page footer">
+        <span className="smaller">Grab a camera</span>
+        <span className="bigger">and go!</span>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            bottom: "8vh",
+            position: "absolute"
+          }}
+        >
+          <button
+            className="button"
+						onClick={() => toggleModal(!isModalOpen)}
+            style={{ marginRight: "20px" }}
+          >
+            moodboard
+          </button>
+          <a href="https://adamgiebl.best" target="_blank">
+            adamgiebl.best
+          </a>
+					<a href="https://github.com/adamgiebl/photography" target="_blank">
+            repository
+          </a>
         </div>
+      </div>
+			<ModalGateway>
+        {isModalOpen &&
+          <Modal onClose={()=>toggleModal(false)}>
+            <Carousel views={[{ src: image }]} />
+          </Modal>
+				}
+      </ModalGateway>
+      
     </footer>
   );
 }
