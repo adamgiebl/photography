85b72363b2946b0c0d8c842ae8ab0c31c39e2569
Adam <adamgiebl@gmail.com>
2020-03-08 21:00:06 +0100

    button

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -6,7 +6,10 @@ function Footer() {
         <div className="page footer">
             <span className="smaller">Grab a camera</span>
             <span className="bigger">and go!</span>
+            <div style={{ display: 'flex', alignItems: 'center', bottom: '8vh', position: 'absolute' }}>
+            <a class="button" href="../images/moodboard.png" style={{ marginRight: '20px' }}>moodboard</a>
             <a href="https://adamgiebl.best" target="_blank">adamgiebl.best</a>
+            </div>
         </div>
     </footer>
   );
